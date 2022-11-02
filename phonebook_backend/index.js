c2cc96a3eb2362f360d42e0249187d631d153029
Juho Kangas <jkangas@student.hive.fi>
2022-11-02 18:13:38 +0200

    rename frontend, finish step 11

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("build"));
 
 morgan.token("body", (req) => JSON.stringify(req.body));
 app.use(
@@ -97,7 +98,7 @@ app.delete("/api/persons/:id", (req, res) => {
 	res.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
