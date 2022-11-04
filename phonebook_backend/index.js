c9da62a79bad3124397d99673c58671b796fb1d5
Juho Kangas <jkangas@student.hive.fi>
2022-11-04 14:47:20 +0200

    fix eslint errors with indentation

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -12,104 +12,102 @@ app.use(express.static('build'))
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(
-    morgan(
-        ':method :url :status :res[content-length] - :response-time ms :body'
-    )
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
 )
 
 app.get('/api/persons', (req, res) => {
-    Person.find({}).then((people) => {
-        res.json(people)
-    })
+  Person.find({}).then((people) => {
+    res.json(people)
+  })
 })
 
 app.get('/api/info', (req, res) => {
-    Person.find({}).then((people) => {
-        const peopleAmount = people.length
-        const timeStamp = new Date()
-        res.send(
-            `<p>Phonebook has info for ${peopleAmount} people</p><br />${timeStamp}`
-        )
-    })
+  Person.find({}).then((people) => {
+    const peopleAmount = people.length
+    const timeStamp = new Date()
+    res.send(
+      `<p>Phonebook has info for ${peopleAmount} people</p><br />${timeStamp}`
+    )
+  })
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id
-    Person.findById(id)
-        .then((person) => {
-            if (person) {
-                res.json(person)
-            } else {
-                res.status(404).end()
-            }
-        })
-        .catch((error) => next(error))
+  const id = req.params.id
+  Person.findById(id)
+    .then((person) => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
 })
 
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    const person = new Person({
-        name: body.name,
-        number: body.number,
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person
+    .save()
+    .then((savedPerson) => {
+      res.json(savedPerson)
     })
-
-    person
-        .save()
-        .then((savedPerson) => {
-            res.json(savedPerson)
-        })
-        .catch((error) => next(error))
+    .catch((error) => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const { name, number } = req.body
+  const { name, number } = req.body
 
-    Person.findByIdAndUpdate(
-        req.params.id,
-        { name, number },
-        {
-            new: true,
-            runValidators: true,
-            context: 'query',
-        }
-    )
-        .then((updatedPerson) => {
-            res.json(updatedPerson)
-        })
-        .catch((error) => next(error))
+  Person.findByIdAndUpdate(
+    req.params.id,
+    { name, number },
+    {
+      new: true,
+      runValidators: true,
+      context: 'query',
+    }
+  )
+    .then((updatedPerson) => {
+      res.json(updatedPerson)
+    })
+    .catch((error) => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id
-    Person.findByIdAndRemove(id)
-        .then(() => {
-            res.status(204).end()
-        })
-        .catch((error) => next(error))
+  const id = req.params.id
+  Person.findByIdAndRemove(id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'Malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'Malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- phonebook_backend/models/person.js
+++ phonebook_backend/models/person.js
@@ -3,38 +3,38 @@ const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI
 
 mongoose
-    .connect(url)
-    .then(() => {
-        console.log('connected to MongoDB')
-    })
-    .catch((err) => {
-        console.log('Error connecting to MongoDB:', err.message)
-    })
+  .connect(url)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((err) => {
+    console.log('Error connecting to MongoDB:', err.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /\d{2}-\d{6,}/.test(v) || /\d{3}-\d{5,}/.test(v)
+      },
+      message: (props) => `${props.value} is not a valid phone number`,
     },
-    number: {
-        type: String,
-        validate: {
-            validator: function (v) {
-                return /\d{2}-\d{6,}/.test(v) || /\d{3}-\d{5,}/.test(v)
-            },
-            message: (props) => `${props.value} is not a valid phone number`,
-        },
-        required: true,
-    },
+    required: true,
+  },
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedbject) => {
-        returnedbject.id = returnedbject._id.toString()
-        delete returnedbject._id
-        delete returnedbject.__v
-    },
+  transform: (document, returnedbject) => {
+    returnedbject.id = returnedbject._id.toString()
+    delete returnedbject._id
+    delete returnedbject.__v
+  },
 })
 
 module.exports = mongoose.model('Person', personSchema)
